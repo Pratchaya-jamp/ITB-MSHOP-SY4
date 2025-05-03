3dde1387c9b30d39273e2ad56432a73565a9a210
Pratchaya <alone9364@gmail.com>
2025-05-03 19:42:25 +0700

    modified vite config for run dev

--- itb-front/vite.config.js
+++ itb-front/vite.config.js
@@ -17,4 +17,10 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    host: '0.0.0.0',   // <<< สำคัญ: เพื่อให้ container ฟังจากทุกที่
+    port: 5173,
+    strictPort: true,
+    allowedHosts: ['ip24sy4.sit.kmutt.ac.th'],
+  }
 })
