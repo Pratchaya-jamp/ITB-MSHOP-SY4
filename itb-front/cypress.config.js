96a764804732e8ced6d166dc2f1a8f5d921bf63c
Pratchaya-jamp <alone9364@gmail.com>
2025-05-03 18:30:23 +0700

    update cypress test

---
+++ itb-front/cypress.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
+    baseUrl: 'http://ip24sy4.sit.kmutt.ac.th:5173',
+    baseAPI: 'http://ip24sy4.sit.kmutt.ac.th:8080/itb-mshop',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
