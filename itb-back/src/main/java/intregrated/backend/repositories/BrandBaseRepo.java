16fb46512fd4da2c4b6b2d10a25c11335900e724
yuzu <142778020+ayeyuzu34@users.noreply.github.com>
2025-05-03 21:15:42 +0700

    Delete itb-back directory

--- itb-back/src/main/java/intregrated/backend/repositories/BrandBaseRepo.java
+++
@@ -1,7 +0,0 @@
-package intregrated.backend.repositories;
-
-import intregrated.backend.entities.BrandBase;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BrandBaseRepo extends JpaRepository<BrandBase, Integer> {
-}
