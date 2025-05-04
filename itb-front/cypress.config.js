11cd372e3cbc48450f204065ea16c5043ec3bf0a
Pratchaya-jamp <alone9364@gmail.com>
2025-05-04 11:22:36 +0700

    ีupdate baseAPI

--- itb-front/cypress.config.js
+++ itb-front/cypress.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     baseUrl: 'http://ip24sy4.sit.kmutt.ac.th:5173',
-    baseAPI: 'http://ip24sy4.sit.kmutt.ac.th:8080/itb-mshop',
+    baseAPI: 'http://ip24sy4.sit.kmutt.ac.th:8080',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
