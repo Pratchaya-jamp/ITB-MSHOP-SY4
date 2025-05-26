664cd1875f292828a1c85d06bfc5aac6ab2b9e53
Pratchaya-jamp <alone9364@gmail.com>
2025-05-26 15:22:29 +0700

    update cypress sprint 4

--- itb-front/cypress.config.js
+++ itb-front/cypress.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     baseUrl: 'http://intproj24.sit.kmutt.ac.th/sy4/',
-    baseAPI: 'http://intproj24.sit.kmutt.ac.th/sy4/api',
+    baseAPI: 'http://intproj24.sit.kmutt.ac.th/sy4/itb-mshop',
     experimentalRunAllSpecs: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
