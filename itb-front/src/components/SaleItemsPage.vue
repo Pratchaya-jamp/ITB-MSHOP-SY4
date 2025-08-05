0eaa75af982b037e33fbe84ea3127b20ccf02bb1
pawin nacharean <pawin.nach@mail.kmutt.ac.th>
2025-08-05 15:05:35 +0700

    update fetchutilsour
    update fetchutilsour

--- itb-front/src/components/SaleItemsPage.vue
+++ itb-front/src/components/SaleItemsPage.vue
@@ -618,8 +618,8 @@ watch(
         class="itbms-bg fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center"
       >
         <div class="bg-white text-black rounded-lg p-6 shadow-lg text-center">
-          <h2 class="text-xl font-semibold mb-4">Error 500!</h2>
-          <p class="itbms-message mb-4">The sale item has been Fail added!</p>
+          <h2 class="text-xl font-semibold mb-4">The sale item has been Fail added!</h2>
+          <p class="itbms-message mb-4">Please try again later.</p>
           <button @click="closeSuccessPopup" class="bg-blue-500 text-white border-2 border-blue-500 rounded-md px-4 py-2 cursor-pointer transition-colors duration-300 hover:bg-transparent hover:text-blue-500">Done</button>
         </div>
       </div>
