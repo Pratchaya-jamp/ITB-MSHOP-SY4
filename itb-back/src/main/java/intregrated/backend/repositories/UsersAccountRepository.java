ad482dee8a5d32b3476998385b569200eeea8752
yuzu <142778020+ayeyuzu34@users.noreply.github.com>
2025-08-25 15:43:32 +0700

    118 - [update] register update api spce

--- itb-back/src/main/java/intregrated/backend/repositories/UsersAccountRepository.java
+++ itb-back/src/main/java/intregrated/backend/repositories/UsersAccountRepository.java
@@ -4,4 +4,5 @@ import intregrated.backend.entities.UsersAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsersAccountRepository extends JpaRepository<UsersAccount, Integer> {
+    boolean existsByEmail(String email);
 }
